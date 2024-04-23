fd4dcfecff32db3ff1012941043009c9136cb48f
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-04-23 10:38:25 +0200

    Add: Get reviews endpoint added.

--- index.js
+++ index.js
@@ -5,16 +5,48 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
+app.get("/reviews", async (req, res) => {
+    fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${process.env.PLACE_NAME}&inputtype=textquery&fields=place_id&key=${process.env.GOOGLE_API_KEY}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const placeId = data.candidates[0].place_id;
+
+            fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews&key=${process.env.GOOGLE_API_KEY}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    return res.status(200).json({
+                        success: true,
+                        data: data.result.reviews,
+                    });
+                })
+                .catch((error) => {
+                    console.log(error);
+                    return res.status(500).json({
+                        success: false,
+                        message: "Internal server error",
+                    });
+                });
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.status(500).json({
+                success: false,
+                message: "Internal server error",
+            });
+        });
+});
+
 try {
     app.listen(process.env.PORT || 3000, () => {
         console.log(`Server listening on port ${process.env.PORT || 3000}`);
     });
 } catch (error) {
-    console.log(error);
+    console.log(`Server inizialization error: ${error}`);
 }
